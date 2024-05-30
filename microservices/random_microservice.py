25998254f30fd9383bd5d818bcebe03fb99e6b2b
Adrian Olives <AdrianOlives975@gmail.com>
2024-05-30 01:07:24 -0700

    Some messing around

---
+++ microservices/random_microservice.py
@@ -0,0 +1,15 @@
+import random
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/generate", methods=["GET"])
+def generate_random_number():
+    random_number = random.randint(1, 1000)
+    return jsonify({"random_number": random_number})
+
+
+if __name__ == "__main__":
+    app.run(port=8001)
